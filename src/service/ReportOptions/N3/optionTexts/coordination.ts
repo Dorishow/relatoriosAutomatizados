756df127a767773f323720bf2f46fb12ac245550
Dorishow <jsantosalmeida54@gmail.com>
2022-04-26 19:25:52 -0300

    feat: coordination options

--- src/service/ReportOptions/N3/optionTexts/coordination.ts
+++ src/service/ReportOptions/N3/optionTexts/coordination.ts
@@ -2,7 +2,19 @@ import { performance } from '../../../../enumerator/performance';
 import { subjectOptions } from '../../../../model/rating/ratingOption';
 
 export const coordinationOptions: subjectOptions = {
-  [performance.LOW]: ['coord low 1', 'coord low 2', 'coord low 3'],
-  [performance.MEDIUM]: ['coord medium 1', 'coord medium 2', 'coord medium 3'],
-  [performance.HIGH]: ['coord high 1', 'coord high 2', 'coord high 3'],
+  [performance.LOW]: [
+    'Quanto à sua coordenação motora, apresenta algumas dificuldades, mas tem avançado consideravelmente.',
+    'Com relação à sua coordenação motora, ainda apresenta dificuldades, mas tem alcançado bons resultados.',
+    'Em relação à sua coordenação motora, apesar de apresentar dificuldade com algumas consoantes, tem alcançado bons avanços.',
+  ],
+  [performance.MEDIUM]: [
+    'Quanto à sua coordenação motora, tem conseguido avançar e performar sem dificuldades até o momento.',
+    'Com relação à sua coordenação motora, seu desempenho tem sido muito proveitoso, performando com pouca ou nenhuma dificuldade.',
+    'Em relação à sua coordenação motora, têm realizado as atividades e desempenhado sem dificuldades.',
+  ],
+  [performance.HIGH]: [
+    'Quanto à sua coordenação motora, seu desempenho é exemplar.',
+    'Com relação à sua coordenação motora, tem desempenhado com facilidade, fazendo as atividades sem dificuldades',
+    'Em relação à sua coordenação motora, seu desempenho é excelente, realizando as atividades em sala de aula de modo exemplar',
+  ],
 };
